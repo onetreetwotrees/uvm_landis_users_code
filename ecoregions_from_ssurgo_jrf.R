edd921e0557d58da63c815b7e56918a8dff2abc9
onetreetwotrees <janerfoster@gmail.com>
2021-03-10 23:48:30 -0500

    Several edits to ecoregions_from_ssurgo_jrf.R to simplify, make compatible with VT-MA landscape. Initial commit for other R code and data input/outputs.

--- ecoregions_from_ssurgo_jrf.R
+++ ecoregions_from_ssurgo_jrf.R
@@ -6,10 +6,14 @@ library(Hmisc)
 library(plyr)
 library(dplyr)
 
-options(tibble.width = Inf)
+#options(tibble.width = Inf)
+options(dplyr.width = Inf,scipen=999, stringsAsFactors = F) # scipen=0 to turn scientific notation back on again.
+
 # Read SR SSURGO
 ## CHECK PATH
-D2 <- read.csv("data_shared\\Hanusia_files_for_soil_class_analysis-20210303\\soilmu_a_Merge_26Feb2021.csv",header=T)
+#D2 <- read.csv("data\\soilmu_a_Merge_26Feb2021.csv",header=T)
+D2h <- read.csv("data\\mu_summary_wilt_hanusia_2021-03-10.csv", header=T)
+D2 <- read.csv("data\\mu_summary_VT-MA.csv", header=T)
 ## Problem with mean elevation. Steps: (1) Open shape file and DEM in Arcmap. (2) Go to catalog, spatial analyst, zonal stats by table.
 ## (3) Specify shapefile for first entry, use FID as the field to summarize zonal stats. Specify DEM as the raster to summarize.
 ## Compute just the MEAN and tell it to ignore null or nodata values. It will probably default to writing the output to 
@@ -19,38 +23,37 @@ D2 <- read.csv("data_shared\\Hanusia_files_for_soil_class_analysis-20210303\\soi
 ## I don't know why, could be a problem with Shapefile? Maybe try this and see if you can figure it out. I put the topographic
 ## indices raster stack in the share drive data_Hanusia. Elevation is the first band of that raster stack.
 #elev <- read.csv("data_shared\\Hanusia_files_for_soil_class_analysis-20210303\\Benn-Berk_soilmu_a_Merge_mean_elev_by_FID_zonal_stats.csv",header=T)
+elev <- read.csv("data\\soil_mu_a_vtma_mu_summary_elev_by_oid.txt", header=T) # "VALUE" relates to objectID in merged shapefile of ssurgo soils data.
 
 # Edit some field names and formats - specific to Hanusia's table
 names(D2)
 #[1] "ï..OID"     "AREASYMBOL" "SPATIALVER" "MUSYM"      "MUKEY"      "Shape_Leng" "Shape_Area" "MUKEY2"     "mukey_1"    "FirstOfslo"
 #[11] "FirstOfbro" "SumOfksat_" "SumOfsand_" "SumOfsilt_" "SumOfclay_" "SumOffield" "SumOfmaxde" "FirstOftex" "FirstOft_1" "FirstOft_2"
 #[21] "AvgOfwfift" "AvgOfwfi_1" "AvgOfwfi_2"
-names(D2)[1] <- "OID"
+#names(D2)[1] <- "OID"
 
 # Hanusia - To DO: Rename or reformat columns to the names specified in the next lines. Your starting column names are not the same,
 # So you'll have to read through and update to agree with your column names. I'm not sure why yours got truncated, problem in Arcmap?
 # You may not have to cast them to character or numeric if they already have that format, but the desired format is as.numeric, when specified.
+## Simplify some variable names and make sure they are classified as.numeric
 D2$slopegradw <- D2$FirstOfslo
-D2$slopegradw <- as.numeric(as.character(D2$FirstOfslopegradwta))
-D2$brockdepmi <- as.numeric(as.character(D2$FirstOfbrockdepmin))
-D2$ksat <- as.numeric(as.character(D2$SumOfksat_weight1))
-D2$sand <- as.numeric(as.character(D2$SumOfsand_weight1))
-D2$silt <- as.numeric(as.character(D2$SumOfsilt_weight1))
-D2$clay <- as.numeric(as.character(D2$SumOfclay_weight1))
-D2$fieldcap <- as.numeric(as.character(D2$SumOffieldcap_weight1))
-D2$maxdepth <- as.numeric(as.character(D2$SumOfmaxdepth_weight))
+D2$slopegradw <- D2$FirstOfslopegradwta # as.numeric()
+D2$brockdepmi <- as.numeric(D2$FirstOfbrockdepmin)
+D2$ksat <- as.numeric(D2$SumOfksat_weight1)
+D2$sand <- as.numeric(D2$SumOfsand_weight1)
+D2$silt <- as.numeric(D2$SumOfsilt_weight1)
+D2$clay <- as.numeric(D2$SumOfclay_weight1)
+D2$fieldcap <- as.numeric(D2$SumOffieldcap_weight1)
+D2$wiltpoint <- as.numeric(D2$SumOfwiltpt_weight1)
+D2$maxdepth <- as.numeric(D2$SumOfmaxdepth_weight)
+## Calculate sum of sand, silt and clay, divide and multiply times 100 to get a percentage of each.
 D2$compsum <- D2$sand + D2$silt + D2$clay
 D2$sand <- D2$sand / D2$compsum * 100
 D2$silt <- D2$silt / D2$compsum * 100
 D2$clay <- D2$clay / D2$compsum * 100
+## Create empty variable "TextClass" to be populated by rule-based classifier below
 D2$TextClass <- "None"
-D2 <- D2[D2$MUSYM != "W",]
 attach(D2)
-
-# Extract cut points from the soils data for your study area. These will be used to bin/subdivide your soils classes later.
-# Current quantiles divide slope and depth observed in your soils data into 1/3s, but you can adjust to different bins.
-slopecuts <- quantile(D2$slopegradw,probs=seq(0,1,1/3),na.rm=T)[2:3]
-depthcuts <- quantile(D2$maxdepth,probs=seq(0,1,1/3),na.rm=T)[2:3]
 
 # Calc texture classes - This is the section in the code that creates soil texture classes from thresholds in sand, silt, clay
 D2$TextClass[silt+(1.5*clay) < 15] <- "Sand"
@@ -68,53 +71,51 @@ D2$TextClass[((clay >=35)&(sand>45))] <- "SandyClay"
 D2$TextClass[((clay >=40)&(silt>=40))] <- "SiltyClay"
 D2$TextClass[((clay >=40)&(sand<=45)&(silt<40))] <- "Clay"
 
-# Apply cut points. Cutpoints are the quantiles you defined about in Extract cut points section.
-D2$slopebin <- as.numeric(cut2(D2$slopegradw,unique(slopecuts)))
-D2$depthbin <- as.numeric(cut2(D2$maxdepth,unique(depthcuts)))
+## Quick vizualize barplot of TextClass in your data
+barplot(table(D2$TextClass))
 
-# Identify unique combinations. These will become your soils classes. This may be preliminary if you want to substitute elevation as a divider.
-Dout <- plyr::count(D2[,c("depthbin","TextClass","slopebin")])
 
-# Ecoregion index
-Dout$Index <- row.names(Dout)
+## Link elevation data from shapefile back to mu_summary table by mukey
+elev$elev <- elev$MEAN
+## Join you merged shapefile attribute table (imported as "elev" here) with table D2, excluding any overlapping variables
+elev2 <- elev %>% dplyr::select(!(which(names(elev) %in% names(D2)))) %>% inner_join(D2, by = c("MUKEY" = "mukey"))
 
-# Apppend ecoregion classes to SSURGO data
-Dcomb <- merge(D2,Dout,by=c("TextClass","depthbin","slopebin"))
-## CHECK PATH
-#outfile <- "C:/BRM/LANDIS_II/Projects/SavageRiver/savageriver_eco_index_031717.csv"
-#write.csv(Dcomb, file=outfile,row.names=FALSE)
-
-adkmerge <- read.csv("C:\\Users\\janer\\Dropbox\\Projects\\Adirondacks\\gis\\adk_soil_index_merget.csv",header=T)
-names(adkmerge)
-
-elev2 <- elev %>% dplyr::select("oid","mean") %>% dplyr::rename(elev = mean)
-
-adk2 <- adkmerge %>% inner_join(elev2,by=c("OBJECTID"="oid"))
-
-# Figure out new cut points with elevation, just for study area MUSYM
-slopecuts2 <- quantile(adk2$slopegradw,probs=seq(0,1,1/3),na.rm=T)[3]
-depthcuts2 <- quantile(adk2$maxdepth,probs=seq(0,1,0.2),na.rm=T)[2:3]
-depthcuts2 <- c(30)
-elevcuts2 <- quantile(adk2$elev,probs=seq(0,1,0.1))[c(6,10)]
+# Figure out cut points to subdivide soil texture classes based on slope, depth or with elevation, if you calculated zonal means by oid
+slopecuts <- quantile(elev2$slopegradw,probs=seq(0,1,1/3),na.rm=T)[3]
+depthcuts <- quantile(elev2$maxdepth,probs=seq(0,1,0.2),na.rm=T)[2:3]
+#depthcuts <- c(30)
+# Vizualize zoil depth
+with(elev2, hist(maxdepth, nclass=40)) # If most soils are deeper than 40cm, where most root action is, maybe not worth using as divider
+elevcuts <- quantile(elev2$elev,probs=seq(0,1,0.1), na.rm=T)[c(6,10)]
+## For New England, when dividing elevation into thirds, consider using 650 as upper cut, as it approximates mean for ecotone
+elevcuts <- c(elevcuts[1],650)
 
 # Apply cut points
-adk2$slopebin2 <- as.numeric(cut2(adk2$slopegradw,unique(slopecuts2)))
-adk2$depthbin2 <- as.numeric(cut2(adk2$maxdepth,unique(depthcuts2)))
-adk2$elevbin2 <- as.numeric(cut2(adk2$elev,unique(elevcuts2)))
+elev2$slopebin <- as.numeric(cut2(elev2$slopegradw,unique(slopecuts)))
+elev2$depthbin <- as.numeric(cut2(elev2$maxdepth,unique(depthcuts)))
+elev2$elevbin <- as.numeric(cut2(elev2$elev,unique(elevcuts)))
 
 # Identify unique combinations
-Dout <- plyr::count(adk2[,c("depthbin2","texcl","elevbin2")])
+## In this step you can add more of the cut point variables, like slopebin or depthbin to create more classes if desired
+Dout <- plyr::count(elev2[,c("TextClass","elevbin")])
+print(Dout)
+
+sumArea <- elev2 %>% group_by(TextClass, elevbin) %>% summarize_at(vars(Shape_Area), list(~ sum(.,na.rm=T)))
+names(sumArea)[3] <- "sum_Shape_Area"
 
 # Ecoregion index
-Dout$soilindex2 <- row.names(Dout)
+Dout$soilindex <- row.names(Dout)
+Dout <- Dout %>% inner_join(sumArea)
 
-# Apppend ecoregion classes to SSURGO data
-Dcomb <- merge(adk2,Dout,by=c("texcl","depthbin2","elevbin2"))
+# Append ecoregion classes to SSURGO data
+Dcomb <- merge(elev2,Dout,by=c("TextClass","elevbin"))
+# Compute available water holding capacity (AWC or WHC) as difference between Field Capacity and Wilting Point
+Dcomb$WHC <- Dcomb$fieldcap - Dcomb$wiltpoint
 
 # Compute weighted mean of soil variables (weighted by polygon area)
-Dout2 <- Dcomb %>% group_by(soilindex2) %>% summarize_at(vars(fieldcap,slopegradw,brockdepmi,ksat,sand,silt,clay,maxdepth,elev),funs(weighted.mean(., Shape_Area,na.rm=T)))
-Dout3 <- Dout %>% inner_join(Dout2) %>% dplyr::select(soilindex2,texcl,depthbin2,elevbin2,elev,freq,fieldcap:maxdepth)
-Dcombout <- Dcomb %>% dplyr::select(OBJECTID,elev,slopebin2,elevbin2,depthbin2,soilindex2) %>%
+Dout2 <- Dcomb %>% group_by(soilindex) %>% summarize_at(vars(fieldcap,wiltpoint,WHC,slopegradw,brockdepmi,ksat,sand,silt,clay,maxdepth,elev),list(~ weighted.mean(., Shape_Area,na.rm=T)))
+Dout3 <- Dout %>% inner_join(Dout2) %>% dplyr::select(soilindex,TextClass,elevbin,elev,freq,fieldcap:maxdepth)
+Dcombout <- Dcomb %>% dplyr::select(OBJECTID,elev,elevbin,soilindex) %>%
             arrange(OBJECTID)
 soilindex2remap <- Dcombout$soilindex2
 soilindex2remap[which(soilindex2remap == 4)] <- 5

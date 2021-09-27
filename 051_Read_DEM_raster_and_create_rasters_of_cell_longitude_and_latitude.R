cdb8c85711e3b66d1c25beadf0ad460eeb225ee4
onetreetwotrees <janerfoster@gmail.com>
2021-09-27 14:07:22 -0400

    Added some code to read in the shapefile of soils polygons from SSURGO and then rasterize them using the DEM elevation raster as a template raster. Then reclassified values in soils raster so that the northern portion would add 100. Remasked so inactive areas equal 0. Changed projection so there is an unprojected raster in WGS84 that can be used efficiently in Google Earth enggine to summarize gridded climate data by soils class.

--- 051_Read_DEM_raster_and_create_rasters_of_cell_longitude_and_latitude.R
+++ 051_Read_DEM_raster_and_create_rasters_of_cell_longitude_and_latitude.R
@@ -5,6 +5,8 @@ library(sp)
 
 ## Read in raster of DEM
 dem <- raster('File directory//path//filename')
+soil <- readOGR(dsn = "C://Users//janer//Dropbox//Projects//Inspires//data_shared//Soil class outputs AUG 2021-20210915T170256Z-001//Soil class outputs AUG 2021",
+               layer = "soilmu_a_Merge_colors_25Aug2021")
 #dem01 <- raster('C://Users//janer//Dropbox//Projects//Inspires//data_download//GEE//Topographic_Indices_Hanusia_from_SRTM_utm18_WGS84-0000000000-0000000000.tif',
 #                band = 1)
 #dem02 <- raster('C://Users//janer//Dropbox//Projects//Inspires//data_download//GEE//Topographic_Indices_Hanusia_from_SRTM_utm18_WGS84-0000011008-0000000000.tif',
@@ -13,7 +15,22 @@ dem <- raster('File directory//path//filename')
 ## Write mosaicked DEM to file.
 #writeRaster(dem, 'C://Users//janer//Dropbox//Projects//Inspires//data_download//GEE//DEM_Hanusia_from_SRTM_utm18_WGS84.tif', 
 #            format = "GTiff")
-  
+
+## Get the map projection info for dem raster and save as crs object
+crs.dem <- crs(dem)  
+## Reproject soil polygons to same map projection as dem using crs object
+## First, make sure you have a numeric field of class numbers to rasterize
+soil$soilIndex2 <- as.numeric(soil$solndx2)
+
+soil.crs.dem <- spTransform(soil, crs.dem)
+## Rasterize soil polygons to same extent and resolution as dem raster
+soil.Rast <- rasterize(soil.crs.dem, dem, field = 'soilIndex2', background = 0)
+## Example of how to write a modified raster to new Geotiff file.
+## Change variable name, directory path and new file name for your application.
+writeRaster(soil.Rast, 'C://Users//janer//Dropbox//Projects//Inspires//data_shared//Soil class outputs AUG 2021-20210915T170256Z-001//Soil class outputs AUG 2021//Hanusia_soilmu_a_Merge_colors_25Aug2021.tif', 
+            format = "GTiff")
+
+
 ## Create function to extract lonlat from raster cell
 ## Function copied from  https://github.com/SWotherspoon/SGAT/blob/master/R/Raster.R
 
@@ -58,8 +75,28 @@ dem.class[which(dem.lat[] > lat.divide)] <- 5 + dem.class[which(dem.lat[] > lat.
 ## Now plot the result
 plot(dem.class)
 
+## Now try this process on existing soils class raster rasterized above.
+soil.Rast2 <- soil.Rast
+soil.Rast2[which(dem.lat[] > lat.divide)] <- 100 + soil.Rast[which(dem.lat[] > lat.divide)]
+
+## Now plot the result
+plot(soil.Rast2)
+
+## Now re-mask the background
+soil.Rast2[which(soil.Rast[] == 0)] <- 0
+## Checking the datatype before writing out to new raster
+dataType(soil.Rast2)
+? datatype
+## Now transform map projection to .prj most compatible with Google Earth Engine
+soil.Rast2.wgs84 <- projectRaster(soil.Rast2, crs = CRS("+init=epsg:4326"))
+
 ## Example of how to write a modified raster to new Geotiff file.
 ## Change variable name, directory path and new file name for your application.
-writeRaster(dem.class, 'C://Users//janer//Dropbox//Projects//Inspires//data_download//GEE//DEM_Hanusia_from_SRTM_utm18_WGS84_class_example.tif', 
-            format = "GTiff")
+##writeRaster(dem.class, 'C://Users//janer//Dropbox//Projects//Inspires//data_download//GEE//DEM_Hanusia_from_SRTM_utm18_WGS84_class_example.tif', 
+##            format = "GTiff")
+writeRaster(soil.Rast2, 'C://Users//janer//Dropbox//Projects//Inspires//data_shared//Soil class outputs AUG 2021-20210915T170256Z-001//Soil class outputs AUG 2021//Hanusia_soilmu_a_Merge_colors_25Aug2021_add100_utm18N.tif', 
+            format = "GTiff", datatype = 'INT2U') ## Save to raster file, specify unsigned integer dataType
+## Write out the copy of modified soils raster in WGS84 datum. Use this one for GEE climate summaries.
+writeRaster(soil.Rast2.wgs84, 'C://Users//janer//Dropbox//Projects//Inspires//data_shared//Soil class outputs AUG 2021-20210915T170256Z-001//Soil class outputs AUG 2021//Hanusia_soilmu_a_Merge_colors_25Aug2021_add100_wgs84.tif', 
+            format = "GTiff", datatype = 'INT2U') ## Save to raster file, specify unsigned integer dataType
 
